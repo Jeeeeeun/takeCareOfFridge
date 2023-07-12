8040e030d11d71f50f07c3bc594a5e941e351ff9
hyeeok <dlgur57@gmail.com>
2023-07-12 17:03:35 +0900

    login complete

--- src/main/java/com/frg/controller/LoginController.java
+++ src/main/java/com/frg/controller/LoginController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.frg.domain.UserDTO;
 import com.frg.service.LoginService;
+import com.frg.util.SHAEncodeUtil;
+import com.frg.util.SessionUtil;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,41 +22,43 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class LoginController {
 
-    @Setter(onMethod_ = @Autowired)
-    LoginService service;
+	@Setter(onMethod_ = @Autowired)
+	LoginService service;
 
-    @GetMapping(value="/login")
-    public String getLogin() {
-        return "/frg/login";
-    }
+	@GetMapping(value = "/login")
+	public String getLogin() {
+		return "/frg/login";
+	}
 
-    @PostMapping(value="/login")
-    public String postLogin(HttpServletRequest request, Model model) {
-        UserDTO dto = new UserDTO();
-        dto.setUser_id(request.getParameter("user_id"));
-        dto.setUser_pw(request.getParameter("user_pw"));
+	public static final int LOGIN_SECCESS = 1;
 
-        int loginAuth = service.getCountUser(dto);
-        if (loginAuth == 1) {
-            // session binding
-            service.getUserByIdAndPwd(dto);
-            log.info("성공");
-            return "/frg/login";
+	@PostMapping(value = "/login")
+	public String postLogin(HttpServletRequest request, Model model) {
+		UserDTO dto = new UserDTO();
+		dto.setUser_id(request.getParameter("user_id"));
+		dto.setUser_pw(SHAEncodeUtil.encodeSha(request.getParameter("user_pw")));
 
-//            int loginClass = service.getClassUser(dto);
-//            if (loginClass != 0) {
-//                SessionUtil.setSessionAttributes(request, dto.getUser_id(), true, "Y");
-//                return "redirect:/frgListShow";
-//            } else {
-//                SessionUtil.setSessionAttributes(request, dto.getUser_id(), true, "N");
-//                return "redirect:/frgListAdd";
-//            }
-        } else {
-            // 로그인 실패
-            // 로그인 ;
-            model.addAttribute("errorMessage", "로그인 실패했습니다!");
-            log.info("실패");
-            return "/frg/login";
-        }
-    }
+		int loginAuth = service.getCountUser(dto);
+		if (loginAuth == LOGIN_SECCESS) {
+			// session binding
+			service.getUserByIdAndPwd(dto);
+
+			int loginClass = service.getClassUser(dto);
+			if (loginClass != 0) {
+				SessionUtil.setSessionAttributes(request, dto.getUser_id(), true, "Y");
+				log.info("frgListShow");
+				return "redirect:/login";
+			} else {
+				SessionUtil.setSessionAttributes(request, dto.getUser_id(), true, "N");
+				log.info("frgListAdd");
+				return "redirect:/login";
+			}
+		} else {
+			// 로그인 실패
+			// 로그인 ;
+			model.addAttribute("errorMessage", "로그인 실패했습니다!");
+			log.info("실패");			
+			return "/frg/login";
+		}
+	}
 }
