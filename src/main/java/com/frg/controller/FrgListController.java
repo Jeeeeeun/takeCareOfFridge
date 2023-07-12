6c3a01809f0a328e67837b9108eb6f83e711629a
Jieun Kim <ryan43778@gmail.com>
2023-07-12 14:19:19 +0900

    FrgList add(80%)

--- src/main/java/com/frg/controller/FrgListController.java
+++ src/main/java/com/frg/controller/FrgListController.java
@@ -1,8 +1,28 @@
 package com.frg.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.frg.domain.FrgListDTO;
+import com.frg.service.FrgListService;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j;
 
 @Controller
+@RequestMapping("/frgList/*")
+@AllArgsConstructor
+@Log4j
 public class FrgListController {
-
+	
+	@NonNull
+	private FrgListService service;
+	
+	@GetMapping("/add")
+	public FrgListDTO registerFrgList() {
+		
+		return null;
+	}
 }
