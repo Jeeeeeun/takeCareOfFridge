60e320144c819df0570e36c7fa477d3d60037a0a
Jieun Kim <ryan43778@gmail.com>
2023-07-22 20:54:17 +0900

    myPage traffic 기준 update 완료

--- src/main/webapp/resources/js/myPage.js
+++ src/main/webapp/resources/js/myPage.js
@@ -1,5 +1,5 @@
 var currentIndex = 0;
-var frgName, frgShape, hRadio, vRadio, sRadio, frgAstate, frgBstate, aFrozenBtn, aCoolBtn, bFrozenBtn, bCoolBtn, frgInfoChangeBtn, frgCorrectionEndBtn, dangerousStandard, warningStandard, dangerousSpan, warningSpan;
+var frgName, frgShape, hRadio, vRadio, sRadio, frgAstate, frgBstate, aFrozenBtn, aCoolBtn, bFrozenBtn, bCoolBtn, frgInfoChangeBtn, frgCorrectionEndBtn, dangerousStandard, warningStandard, dangerousSpan, warningSpan, trfCorrectionEndBtn, announcement;
 
 window.onload = function () {
 	// DOM 객체 위에서 선언해둔 거 이렇게 onload 안에서 초기화시키면 여러 함수에서 전역변수처럼 쓸 수 있음.
@@ -22,12 +22,14 @@ window.onload = function () {
 	warningStandard = document.getElementById("warningStandard");
 	dangerousSpan = document.getElementById("dangerousSpan");
 	warningSpan = document.getElementById("warningSpan");
+	trfCorrectionEndBtn = document.getElementById("trfCorrectionEndBtn");
+	announcement = document.getElementById("announcement");
 
 	updateFrg(currentIndex);
 	trfStandardShow();
 };
 
-/* SESS_ID 데려오려는 함수 */
+// SESS_ID 데려오려는 함수
 function getUserId() {
   return fetch(contextPath + "/frg/getUserId")
     .then(function (response) {
@@ -184,12 +186,16 @@ function nextFrg() {
 }
 
 // 신호등 기준 값 보여주기, 화면 처음 렌더링 될 때
-function trfStandardShow() {
+function trfStandardShow(dangerStandard, warnStandard) {
 
-	let dangerStandard = trfStandard[0].dangerous_standard;
-	let warnStandard = trfStandard[0].warning_standard;
+    // 첫 렌더링 시에만 값을 할당
+    if (!dangerStandard && !warnStandard) {
+        dangerStandard = trfStandard[0].dangerous_standard;
+        warnStandard = trfStandard[0].warning_standard;
+    }
 	
-	if(dangerStandard >= 0) {
+	// 첫 렌더링이 아니라면 여기서부터 실행됨
+	if(dangerStandard > 0) {
 		dangerousStandard.value = dangerStandard
 		dangerousSpan.textContent= "일 지남";
 	} else {
@@ -198,7 +204,7 @@ function trfStandardShow() {
 		dangerousSpan.textContent= "일 남음";
 	}
 	
-	if(warnStandard >= 0) {
+	if(warnStandard > 0) {
 		warningStandard.value = warnStandard;
 		warningSpan.textContent = "일 지남";
 	} else {
@@ -214,11 +220,16 @@ function trfStandardBtnClicked() {
 		// 수정하기 버튼(펜 모양 아이콘) 숨기기
 		trfChangeBtn.style.display = "none";
 		
+		// 수정 완료 버튼 숨겨져 있던 것 등장
+		trfCorrectionEndBtn.style.display = "flex";
+		
+		// 안내 문구 없을 때 벌려놓은 공간 일시적으로 좁힘
+		announcement.style.height = "2%";
+		
 		// 두 개의 세로 기준선 아래로 안내 문구 등장
 		// (작성하는 숫자는 D-Day 개념입니다. D-10은 유통기한 10일 남음 => -10 작성)
 		// (반드시 왼쪽(dangerous) 숫자가 오른쪽(warning) 숫자보다 큰 정수여야 합니다.)
-		const announcement = document.getElementById("announcement");
-		let mention = "작성 예시1) -10: 유통/소비기한 10일 남음\n작성 예시2) +5: 유통/소비기한 5일 지남\n★ 왼쪽 숫자(위험 판단 기준)가 오른쪽 숫자(경고 판단 기준)보다 커야 합니다."
+		let mention = "작성 예시1) -10: 유통/소비기한 10일 남음\n작성 예시2) +5: 유통/소비기한 5일 지남\n★ 왼쪽 숫자(위험 판단 기준)가 오른쪽 숫자(경고 판단 기준)보다 큰 숫자여야 합니다."
 		const announce = document.createElement('pre'); // pre: 사전에 서식이 지정된(preformatted) 텍스트 태그를 말함. 
 		
 		announce.textContent = mention;
@@ -237,7 +248,7 @@ function trfStandardBtnClicked() {
 		standardWrapper.style.marginLeft = "10%";
 		
 		// 위험 기준 input 태그 조작
-		dangerousStandard.value = "";
+		dangerousStandard.value = trfStandard[0].dangerous_standard;
 		dangerousStandard.disabled = false;
 		dangerousStandard.style.borderStyle = "dashed";
 		dangerousStandard.style.borderWidth = "1px";
@@ -247,7 +258,7 @@ function trfStandardBtnClicked() {
 		dangerousSpan.textContent = "";
 		
 		// 경고 기준 input 태그 조작
-		warningStandard.value = "";
+		warningStandard.value = trfStandard[0].warning_standard;
 		warningStandard.disabled = false;
 		warningStandard.style.borderStyle = "dashed";
 		warningStandard.style.borderWidth = "1px";
@@ -260,55 +271,92 @@ function trfStandardBtnClicked() {
 function trfCorrectionEnd() {
 	// 신호등 내용 수정 완료 버튼 클릭하면
 	
-	// jsp에서 값 가져와서 data라는 변수의 JSON 형태로 저장
-	var data = {
-		"dangerous": parseInt(dangerousStandard.value, 10),
-		"warning": parseInt(warningStandard.value, 10)
-	}
-	// ajax로 값을 서버로 넘겨 수정 및 저장됨
-	$.ajax ({
-		type: "POST",
-		url: `${contextPath}/frg/trfStandardChange`, 
-		contentType: "application/json",
-		data: JSON.stringify(data),
-		dataType: "json",
-		success: function(response) {
-			alert('냉장고 식품 보관 관리 기준이 변경되었습니다.');
-			console.log(data);
-		},
-		error: function(err) {
-			alert('냉장고 식품 보관 관리 기준 변경에 실패했습니다.');
-			if (err.status === 404) {
-		    	alert("요청한 페이지를 찾을 수 없습니다.");
-		    } else if (err.status === 500) {
-		    	alert("서버 내부 오류가 발생했습니다.");
-		    } else {
-		    	alert("error - " + err);
-		    }
-		}
-	});
-	// 기준 input 칸 다시 read-only로
-	dangerousStandard.disabled = true;
-	warningStandard.disabled = true;
-	
-	// input 태그들 위치, 너비, 스타일 원상복귀
-	standardWrapper.style.width = "86%";
-	standardWrapper.style.marginLeft = "16%";
-	
-	dangerousStandard.style.borderStyle = "solid";
-	dangerousStandard.style.borderWidth = "0px";
-	dangerousStandard.style.borderColor = "transparent";
-	
-	warningStandard.style.borderStyle = "solid";
-	warningStandard.style.borderWidth = "0px";
-	warningStandard.style.borderColor = "transparent";
-	
-	// "일 지남", "일 남음" 부분 글씨 재등장
-	trfStandardShow();
-	
-	// 숨겨졌던 수정하기 버튼 등장
-	trfChangeBtn.style.display = "flex";
-	
+	// SESS_ID를 가져오는 함수를 호출
+	getUserId()
+    	.then(function (userId) {
+    
+			// jsp에서 값 가져와서 data라는 변수의 JSON 형태로 저장
+			var data = {
+				user_id: userId,
+				dangerous: parseInt(dangerousStandard.value, 10),
+		        warning: parseInt(warningStandard.value, 10),
+		    };
+      
+      		// 다시 렌더링을 위한 값 지정
+      		dangerousStandard.value = data.dangerous;
+      		warningStandard.value = data.warning;
+      		
+      		// 값 크기 비교 (유효하게 들어갈 수 있는지 검사)
+            if (data.dangerous <= data.warning) {
+                alert("위험 판단 기준이 경고 판단 기준보다 숫자가 커야 합니다. 값을 다시 입력해 주세요.");
+                return;
+            } else if (dangerousStandard.value === '' || warningStandard.value === '') {
+            	alert("숫자가 입력되지 않은 곳이 있습니다. 다시 입력해 주세요.");
+            	return;
+            }
+      		
+			// ajax로 값을 서버로 넘겨 수정 및 저장됨
+			$.ajax ({
+				type: "POST",
+				url: `${contextPath}/frg/trfStandardChange`, 
+				contentType: "application/json",
+				data: JSON.stringify(data),
+				dataType: "json",
+				success: function(response) {
+					// "일 지남", "일 남음" 부분 글씨와 함께 바뀐 데이터로 재등장
+					trfStandardShow(dangerousStandard.value, warningStandard.value);
+
+					// 안내 문구 삭제
+					const announcePre = announcement.querySelector('pre'); // announcement란 id 가진 요소 안에 있는 <pre> 태그
+					if(announcePre) { // 만약 <pre> 태그가 있으면
+						announcement.removeChild(announcePre); // 없애줘
+					}
+					
+					// 안내 문구 없을 때 확보해둔 공간 회복
+					announcement.style.height = "14%";
+				
+					setTimeout(function() { // 시간차 두고 알림창 띄우기
+				        alert('냉장고 식품 보관 관리 기준이 변경되었습니다.');
+				    }, 100);
+				},
+				error: function(err) {
+					alert('냉장고 식품 보관 관리 기준 변경에 실패했습니다.');
+					if (err.status === 404) {
+				    	alert("요청한 페이지를 찾을 수 없습니다.");
+				    } else if (err.status === 500) {
+				    	alert("서버 내부 오류가 발생했습니다.");
+				    } else {
+				    	alert("error - " + err);
+				    }
+				}
+			});
+			
+			// 기준 input 칸 다시 read-only로
+			dangerousStandard.disabled = true;
+			warningStandard.disabled = true;
+			
+			// input 태그들 위치, 너비, 스타일 원상복귀
+			standardWrapper.style.width = "86%";
+			standardWrapper.style.marginLeft = "16%";
+			
+			dangerousStandard.style.borderStyle = "solid";
+			dangerousStandard.style.borderWidth = "0px";
+			dangerousStandard.style.borderColor = "transparent";
+			
+			warningStandard.style.borderStyle = "solid";
+			warningStandard.style.borderWidth = "0px";
+			warningStandard.style.borderColor = "transparent";
+			
+			// 숨겨졌던 수정하기 버튼 등장
+			trfChangeBtn.style.display = "flex";
+
+			// 드러나 있던 수정 완료 버튼 숨김
+			trfCorrectionEndBtn.style.display = "none";
+
+			})
+    .catch(function (error) {
+      console.error("사용자 ID를 얻는 데 실패했습니다:", error);
+    });
 }
 
 function frgInfoChangeBtnClicked() {
